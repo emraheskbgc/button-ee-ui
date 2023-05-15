16339aa3bb101c04356b1f4518e8dbb01c46da80
Emrah <emraheskibagci595@gmail.com>
2023-05-15 23:47:48 +0300

    first-my-package1
    ilk paketim

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,24 @@
 import React from 'react'
 
-import { ExampleComponent } from 'button-ee-ui'
+import { Button, Text } from 'button-ee-ui'
 import 'button-ee-ui/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <div>
+      <Button text='Primary Button' type='primary' />
+      <Button text='Default Button' type='default' />
+      <Button text='Dashed Button' type='Dashed' />
+      <br />
+      <Button text='Text Button' type='text' />
+
+      <Button text='Link Button' type='link' />
+      <br />
+      <br />
+      <br />
+      <Text text='There are - primary - button, - default - button, - dashed - button, - text - button and , - link - button in button-ee-ui' />
+    </div>
+  )
 }
 
 export default App
